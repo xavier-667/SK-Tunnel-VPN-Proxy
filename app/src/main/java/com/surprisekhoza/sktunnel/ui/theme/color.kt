2cdf284d6c73e2a6c0563598a7a5ae7ee3c740e2
xavier-667 <itexpress224@gmail.com>
2026-05-26 23:52:34 +0200

    Create src/main/java/com/surprisekhoza/sktunnel/ui/theme/color.kt

---
+++ app/src/main/java/com/surprisekhoza/sktunnel/ui/theme/color.kt
@@ -0,0 +1,19 @@
+package com.surprisekhoza.sktunnel.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Npv Tunnel dark backgrounds
+val NpvDark = Color(0xFF1A1A2E)
+val NpvSurface = Color(0xFF16213E)
+
+// HTTP Custom green accent
+val GreenAccent = Color(0xFF00E676)
+val GreenAccentDark = Color(0xFF00C853)
+
+// Text colors
+val TextWhite = Color(0xFFFFFFFF)
+val TextGray = Color(0xFFB0BEC5)
+
+// Status colors
+val ConnectedGreen = Color(0xFF00E676)
+val DisconnectedGray = Color(0xFF757575)
